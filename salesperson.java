f148d572321d99366c0f875abbeb36bc51d8b656
btk5 <teeratat05@gmail.com>
2025-09-22 22:57:04 +0700

    Create salesperson.java

---
+++ salesperson.java
@@ -0,0 +1,17 @@
+public class salesperson {
+    private int salespersonId;
+    private String name;
+ 
+    public salesperson(int var1, String var2) {
+       this.salespersonId = var1;
+       this.name = var2;
+    }
+ 
+    public int getSalespersonID() {
+       return this.salespersonID;
+    }
+ 
+    public String getName() {
+       return this.name;
+    }
+}
